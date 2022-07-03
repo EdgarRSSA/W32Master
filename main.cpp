98f632efb25285c586e12b85cf7304f45ef54d71
EdgarRS <edgar_rrr@outlook.com>
2022-07-02 22:48:13 -0500

    Preapared console app build

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "pch.h"
 
-// & cl.exe /Wall /Zi /EHsc /std:c++20 /FdC:\Users\Edgar\Desktop\W32Master\build\  /FeC:\Users\Edgar\Desktop\W32Master\build\main.exe /FoC:\Users\Edgar\Desktop\W32Master\build\ /Yu"pch.h" /FpC:\Users\Edgar\Desktop\W32Master\build\pch.pch C:\Users\Edgar\Desktop\W32Master\main.cpp
-
-int main(){
-
+/**
+ * @brief Entry Main.
+ *
+ * @return int Exit code.
+ */
+int main()
+{
     std::cout << "Hello win32" << std::endl;
 
-    return 0;
+    return ERROR_SUCCESS;
 }
